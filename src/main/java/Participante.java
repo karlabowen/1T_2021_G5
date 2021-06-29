420a72df13c58361cfe0afdcb8a8e7199811a669
karlabowen <kbowen@espol.edu.ec>
2021-06-29 04:00:35 -0500

    Se crea la clase Participante con sus respectivos atributos luego se declara el constructor y los gettes y setters.

--- src/main/java/Participante.java
+++ src/main/java/Participante.java
@@ -9,5 +9,30 @@
  * @author Karla
  */
 public class Participante {
+    int id;
+    String nombre;
+
+    public Participante(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
     
 }
+  
+
