9fcf7bca0f8feaaa3562a21f29123e2fcaae9938
karlabowen <kbowen@espol.edu.ec>
2021-07-06 16:12:41 -0500

    declaración de atributos y contructor en la clase competencia

--- src/main/java/proyectopoo/Competencia.java
+++ src/main/java/proyectopoo/Competencia.java
@@ -9,6 +9,30 @@ package proyectopoo;
  *
  * @author Karla
  */
+//plantilla para competencia
 public class Competencia {
+    protected String fecha;
+    protected String hora;
+    protected String primerPremio;
+    protected String segundoPremio;
+    protected String tercerPremio;
+    
+    //constructor
+    public Competencia(String fecha, String hora, String primerPremio, String segundoPremio, String tercerPremio) {
+        this.fecha = fecha;
+        this.hora = hora;
+        this.primerPremio = primerPremio;
+        this.segundoPremio = segundoPremio;
+        this.tercerPremio = tercerPremio;
+    }
+    //getters
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+    
     
 }
