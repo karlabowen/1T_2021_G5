6a248ea99aace0c62d099f0eb7f6fc29227a32c5
karlabowen <kbowen@espol.edu.ec>
2021-07-06 15:07:07 -0500

    Clase estudiante y declaración

--- src/main/java/proyectopoo/Estudiante.java
+++ src/main/java/proyectopoo/Estudiante.java
@@ -1,16 +1,9 @@
 
-import proyectopoo.Persona;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Karla
  */
+package proyectopoo;
 public class Estudiante extends Persona {
     private String genero;
     private String Carrera;
