a562f9ade3d9bd52a44ecf1915a3889deb208f75
karlabowen <kbowen@espol.edu.ec>
2021-07-06 15:10:38 -0500

    Clase Estudiante

---
+++ src/main/java/proyectopoo/Persona.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectopoo;
+
+/**
+ *
+ * @author Karla
+ */
+
+    
+    //plantilla para jurado y estudiante
+public class Persona {
+    protected String id;
+    protected String nombre;
+
+    public Persona(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "id=" + id + ", nombre=" + nombre + '}';
+    }
+    
+    
+    
+}
+    
+
