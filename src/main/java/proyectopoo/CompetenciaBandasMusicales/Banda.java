52adcb1987a06f065f8a68e00fc8572ef7321dd2
Gabriel <Gabriel@DESKTOP-9K7A7T6>
2021-07-06 20:56:35 -0500

    Creación de la clase Banda, metodos getters and setter, método ingresoIntegrantes, verficarIntegrantes.

--- src/main/java/proyectopoo/CompetenciaBandasMusicales/Banda.java
+++ src/main/java/proyectopoo/CompetenciaBandasMusicales/Banda.java
@@ -4,10 +4,67 @@
  * and open the template in the editor.
  */
 package proyectopoo.CompetenciaBandasMusicales;
-/**
- *
- * @author Karla
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+import proyectopoo.Estudiante;
+import proyectopoo.Proyectopoo;
+
 public class Banda {
+    private int idBanda;
+    private static int idBandaSiguiente; //se obtienen ids en orden para cada banda
+    private String nombre;
+    private String cancion;
+    private int lugar;
+    private ArrayList<IntegranteBanda> integrantes;
     
-}
+    private static Scanner sc = new Scanner(System.in);
+
+    public Banda(String nombre, String cancion) {
+        ++idBandaSiguiente;
+        idBanda = idBandaSiguiente;
+        this.nombre = nombre;
+        this.cancion = cancion;
+        this.integrantes = new ArrayList<>();
+        
+    }
+    //getters
+    public int getIdBanda() {
+        return idBanda;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setLugar(int lugar) {
+        this.lugar = lugar;
+    }
+    //ingresa numerode integrates que el usuario desea
+    public String ingresoIntegrantes(int num){
+            for(int i=1; i<=num;i++){
+                System.out.println(verificarIntegrante(i));
+            }
+        return "";
+    }
+      
+    //valida el ingreso de integrante y que cumpla todos los requisistos y si cumple lo agrega
+    private String verificarIntegrante(int i){
+        System.out.println("ID integrante "+i+ ": ");
+        String idEst = sc.nextLine();
+        for(Estudiante e: Proyectopoo.estudiantes){
+            if(e.getId().equals(idEst)){ //compara integrantes
+                IntegranteBanda intBanda = new IntegranteBanda(e); //crea objeto integrante
+                if(contieneIntegrante(intBanda)){ //verifica que no exista
+                   System.out.println("Integrante ya existente");
+                   return verificarIntegrante(i);
+                }
+                //en el caso de que no exista
+                intBanda.setRol(verificaRol()); //agrega el rol
+                addIntegrante(intBanda);
+                return "Estudiante "+ e.getNombre()+" registrado";
+   
+            }
+        }
+        System.out.println("No existe estudiante con ese ID");
+        return verificarIntegrante(i);
+    }
