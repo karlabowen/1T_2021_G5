08388f0a0a752650de30f4da9a2982ca3e07ecb0
karlabowen <kbowen@espol.edu.ec>
2021-07-06 20:00:45 -0500

    Clase Carrera ,declaración de metodos

--- src/main/java/proyectopoo/Carrera5k/Carrera.java
+++ src/main/java/proyectopoo/Carrera5k/Carrera.java
@@ -1,31 +1,56 @@
 package proyectopoo.Carrera5k;
-import java.util.Date;
+import java.util.*;
+import proyectopoo.Competencia;
+import proyectopoo.Estudiante;
+import proyectopoo.Proyectopoo;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
  * @author Karla
  */
-public class Carrera {
-    int Id;
-    Date fecha;
-    Date hora;
-    String premio1l;
-    String premio2l;
-    String premio3l;
-    int cantParticipante=0;
-   public Carrera(int Id, Date fecha, Date hora, String premio1l, String premio2l, String premio3l) {
-        this.Id = Id;
-        this.fecha = fecha;
-        this.hora = hora;
-        this.premio1l = premio1l;
-        this.premio2l = premio2l;
-        this.premio3l = premio3l;
+public class Carrera extends Competencia{
+    private int idCarrera;
+    private static int idCarreraSiguiente; //se obtienen ids en orden para cada carrera
+    private ArrayList<Participante5k> participantes;
+    private ArrayList<Participante5k> ganadores;
+    
+    private static Scanner sc = new Scanner(System.in);
+
+    public Carrera(String fecha, String hora, String primerPremio, String segundoPremio, String tercerPremio) {
+        super(fecha, hora, primerPremio, segundoPremio, tercerPremio);
+        ++idCarreraSiguiente;
+        idCarrera = idCarreraSiguiente;
+        participantes = new ArrayList<Participante5k>() ;
+        ganadores = new ArrayList<Participante5k>();
+    }
+
+
+    public int getIdCarrera() {
+        return idCarrera;
+    }
+
+    public ArrayList<Participante5k> getParticipantes() {
+        return participantes;
+    }
+
+    public ArrayList<Participante5k> getGanadores() {
+        return ganadores;
+    }
+    //registrar paticipantes
+    public String resgistrarParticipantes(){
+        System.out.println("** Registro de participantes en carrera del "+ getFecha()+"  **");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ID del estudiante: ");
+        String idEst = sc.nextLine();
+        while(!idEst.equals("0")){
+
+            System.out.println(verificarIdParticipante(idEst));
+            System.out.println("ID del estudiante: ");
+            idEst = sc.nextLine(); 
+        }
+        return " ";
+
     }
 
     public int getCantParticipante() {
